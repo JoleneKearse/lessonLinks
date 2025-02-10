1d3f0677e8bb220ee8eb11c46a1d8e30aef1b69e
JoleneKearse <pharaohnutz@gmail.com>
2025-02-10 17:54:46 -0500

    chore: update data source for neon

--- server/src/utils/config/data-source.ts
+++ server/src/utils/config/data-source.ts
@@ -5,12 +5,8 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  // url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false, // Use migrations instead in prod
