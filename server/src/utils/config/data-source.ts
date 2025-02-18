dc0532deede8121e9a2aa5ef246681e023d79d12
JoleneKearse <pharaohnutz@gmail.com>
2025-02-18 14:27:30 -0500

    chore: search for entities depending on environment

--- server/src/utils/config/data-source.ts
+++ server/src/utils/config/data-source.ts
@@ -3,11 +3,13 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const isTsEnviro = process.env.NODE_ENV === 'production';
+
 const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   port: Number(process.env.DB_PORT),
-  entities: ['src/**/*.entity.js'],
+  entities: isTsEnviro ? ['src/**/*.entity.ts'] : ['dist/**/*.entity.js'],  
   migrations: ['src/migrations/*.js'],
   synchronize: false, // Use migrations instead in prod
   logging: true,
