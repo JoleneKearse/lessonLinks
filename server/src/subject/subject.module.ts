a37556f16bbcdc24af7ebbdf481b38515ac1be41
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-02-21 17:20:52 -0600

    Prettier formatting fix.

--- server/src/subject/subject.module.ts
+++ server/src/subject/subject.module.ts
@@ -4,6 +4,6 @@ import { SubjectService } from './subject.service.js';
 
 @Module({
   controllers: [SubjectController],
-  providers: [SubjectService]
+  providers: [SubjectService],
 })
 export class SubjectModule {}
