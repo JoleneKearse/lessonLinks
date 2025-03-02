1c6a899062f6f96feaaa7a62991548e9d49217e7
JoleneKearse <pharaohnutz@gmail.com>
2025-03-02 09:41:36 -0500

    chore: remove past migrations and rerun on user and user type

--- server/src/migrations/1740926395011-UpdateCreateUser.ts
+++ server/src/migrations/1740926395011-UpdateCreateUser.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUserType1740774186248 implements MigrationInterface {
-    name = 'CreateUserType1740774186248'
+export class UpdateCreateUser1740926395011 implements MigrationInterface {
+    name = 'UpdateCreateUser1740926395011'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_date" TIMESTAMP WITH TIME ZONE, "userId" uuid NOT NULL, "email" character varying, "provider" character varying NOT NULL, "provider_id" character varying NOT NULL, CONSTRAINT "UQ_d72ea127f30e21753c9e229891e" UNIQUE ("userId"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "UQ_5e3a2b86fd9a9c22c266ae04731" UNIQUE ("provider_id"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
