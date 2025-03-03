d159693f1d8e9b14ab2027324580a7ebb002c28f
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-03-03 16:08:19 -0600

    Added basic endpoints in SubSubjectController and corresponding functions in SubSubjectService.

---
+++ server/src/v1/sub-subject/sub-subject.service.ts
@@ -0,0 +1,40 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
+
+import { NewSubSubjectDTO, SubSubjectEntity } from './sub-subject.entity.js';
+
+@Injectable()
+export class SubSubjectService {
+  constructor(
+    @InjectRepository(SubSubjectEntity)
+    private readonly subSubjectRepository: Repository<SubSubjectEntity>,
+  ) {}
+
+  async findAll(): Promise<SubSubjectEntity[]> {
+    const subSubjects = await this.subSubjectRepository.find();
+    if (subSubjects.length === 0) {
+      throw new NotFoundException('No subSubjects found.');
+    }
+    return subSubjects;
+  }
+
+  async create(newSubSubjectDTO: NewSubSubjectDTO): Promise<SubSubjectEntity> {
+    try {
+      const subSubject = this.subSubjectRepository.create(newSubSubjectDTO);
+      return await this.subSubjectRepository.save(subSubject);
+    } catch (error) {
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('unique constraint')
+      ) {
+        throw new ConflictException('A subject with this name already exists.');
+      }
+      throw error;
+    }
+  }
+}
